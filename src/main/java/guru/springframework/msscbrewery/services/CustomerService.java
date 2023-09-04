62df924ce7e0fca03a96257744610d84a8ac9eb6
mshaffer0308 <mshaffer0308@vt.edu>
2023-09-04 12:01:09 -0600

    Added Customer DTO, controller, service, and implementation

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -0,0 +1,7 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import java.util.UUID;
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID customerId);
+}
